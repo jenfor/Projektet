08463e2d07f86477e99bbce25a23354e0e39ef13
jenfor <jenfor@student.chalmers.se>
2015-02-14 16:00:31 +0100

    CopyPastat material
    Mer kod som bara är kopierad (Någon actionlistener borta)

--- src/Projektet/Game.java
+++ src/Projektet/Game.java
@@ -1,5 +1,17 @@
 package Projektet;
 
-public class Game {
+public class Game
+{
+	private UserInterface gui;
+	private GameEngine engine;
 
+    /**
+     * Create the game and initialise its internal map.
+     */
+    public Game() 
+    {
+		engine = new GameEngine();
+		gui = new UserInterface(engine);
+		engine.setGUI(gui);
+    }
 }
--- src/Projektet/Main.java
+++ src/Projektet/Main.java
@@ -3,7 +3,7 @@ package Projektet;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		new Game();
 
 	}
 
